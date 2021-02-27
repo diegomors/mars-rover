d0f91e5c82597e7dac4184d0d0f7051fc7e5b2e8
Diego Moraes <diegomoraes.a@gmail.com>
2021-02-27 20:18:17 -0300

    initial version to helpers classes

---
+++ src/main/java/helpers/FileHelper.java
@@ -0,0 +1,10 @@
+package helpers;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public class FileHelper {
+    public static List<String> read(String path) throws FileNotFoundException {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/main/java/helpers/ParserHelper.java
@@ -0,0 +1,19 @@
+package helpers;
+
+import entities.Movement;
+import entities.Plateau;
+import entities.Position;
+
+public class ParserHelper {
+    public static Plateau toPlateau(String input) throws IllegalArgumentException {
+        throw new UnsupportedOperationException();
+    }
+
+    public static Position toPosition(String input) throws IllegalArgumentException {
+        throw new UnsupportedOperationException();
+    }
+
+    public static Movement toMovement(String input) throws IllegalArgumentException {
+        throw new UnsupportedOperationException();
+    }
+}
